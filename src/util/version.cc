c5d599e6b0211a4d427fff3a4db8f48d3ab92d0f
Marcin Kościelnicki <koriakin@0x04.net>
2017-03-01 16:08:44 +0100

    Świtezianka

--- src/util/version.cc
+++ src/util/version.cc
@@ -21,10 +21,10 @@ namespace util {
 namespace version {
 
 const unsigned year = 2017;
-const unsigned month = 2;
+const unsigned month = 3;
 const unsigned release = 0;
 const unsigned patch = 0;
-const QString codename = "YAGNI";
+const QString codename = "Świtezianka";
 const QString string = patch ?
   QString("%1.%2.%3.%4.%5").arg(
     QString("%1").arg(year),
