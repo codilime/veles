0d242f7f32c41b8d7f866c121199727da3feca53
Marcin Kościelnicki <koriakin@0x04.net>
2017-03-15 11:33:31 +0100

    Exception classes.

---
+++ python/veles/proto/exceptions.py
@@ -0,0 +1,36 @@
+from veles.compatibility.pep487 import NewObject
+
+
+class VelesException(Exception, NewObject):
+    types = {}
+
+    def __init__(self, *args):
+        if type(self) is VelesException:
+            code, msg = args
+            if code in VelesException.types:
+                self.__class__ = VelesException.types[code]
+                args = msg,
+            else:
+                self.code = code
+        else:
+            code = self.code
+            if args:
+                msg, = args
+            else:
+                msg = self.msg
+        self.msg = msg
+        super(VelesException, self).__init__(*args)
+
+    def __init_subclass__(cls, **kwargs):
+        super(VelesException, cls).__init_subclass__(**kwargs)
+        VelesException.types[cls.code] = cls
+
+
+class ObjectGoneError(VelesException):
+    code = 'object_gone'
+    msg = "Object has been deleted, or never existed"
+
+
+class ObjectExistsError(VelesException):
+    code = 'object_exists'
+    msg = "Object with the given id already exists"
---
+++ python/veles/tests/test_exceptions.py
@@ -0,0 +1,50 @@
+# Copyright 2017 CodiLime
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import unittest
+
+from veles.proto.exceptions import VelesException, ObjectGoneError
+
+
+class TestExceptions(unittest.TestCase):
+    def test_exceptions(self):
+        a = VelesException('zlew', 'Something went wrong.')
+        self.assertEqual(type(a), VelesException)
+        self.assertTrue(isinstance(a, VelesException))
+        self.assertFalse(isinstance(a, ObjectGoneError))
+        self.assertEqual(a.code, 'zlew')
+        self.assertEqual(a.msg, 'Something went wrong.')
+        self.assertEqual(a.args, (a.code, a.msg))
+        a = VelesException('object_gone', 'Object gone.')
+        self.assertEqual(type(a), ObjectGoneError)
+        self.assertTrue(isinstance(a, VelesException))
+        self.assertTrue(isinstance(a, ObjectGoneError))
+        self.assertEqual(a.code, 'object_gone')
+        self.assertEqual(a.msg, 'Object gone.')
+        self.assertEqual(a.args, (a.msg,))
+        a = ObjectGoneError('Whoops.')
+        self.assertEqual(type(a), ObjectGoneError)
+        self.assertTrue(isinstance(a, VelesException))
+        self.assertTrue(isinstance(a, ObjectGoneError))
+        self.assertEqual(a.code, 'object_gone')
+        self.assertEqual(a.msg, 'Whoops.')
+        self.assertEqual(a.args, (a.msg,))
+        a = ObjectGoneError()
+        self.assertEqual(type(a), ObjectGoneError)
+        self.assertTrue(isinstance(a, VelesException))
+        self.assertTrue(isinstance(a, ObjectGoneError))
+        self.assertEqual(a.code, 'object_gone')
+        self.assertEqual(a.msg, ObjectGoneError.msg)
+        self.assertEqual(a.args, ())
