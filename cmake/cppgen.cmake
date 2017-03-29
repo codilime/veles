e8323a44dc721f4488f886e1cbc1e8e3861b2cb7
Jacek Świderski <jacek.swiderski@codilime.com>
2017-03-29 17:06:17 +0200

    Fix broken cmake dependencies after file split

--- cmake/cppgen.cmake
+++ cmake/cppgen.cmake
@@ -6,8 +6,14 @@ endif(WIN32)
 
 set(VENV_DIR ${CMAKE_CURRENT_BINARY_DIR}/msgpack-venv)
 
+if(WIN32)
+  set(PYEXE ${VENV_DIR}/Scripts/python.exe)
+else(WIN32)
+  set(PYEXE ${VENV_DIR}/bin/python)
+endif(WIN32)
+
 add_custom_command(
-  OUTPUT ${VENV_DIR}
+  OUTPUT ${VENV_DIR} ${PYEXE}
   COMMAND ${BASEPYEXE} -m virtualenv ${VENV_DIR})
 
 if(WIN32)
@@ -16,13 +22,10 @@ else(WIN32)
   set(SIX_LOC ${VENV_DIR}/lib/site-packages/six.py)
 endif(WIN32)
 
-if(WIN32)
-  set(PYEXE ${VENV_DIR}/Scripts/python.exe)
-else(WIN32)
-  set(PYEXE ${VENV_DIR}/bin/python)
-endif(WIN32)
-
 add_custom_command(
   OUTPUT ${SIX_LOC}
   COMMAND ${PYEXE} -m pip install six
-  DEPENDS ${VENV_DIR})
+  DEPENDS ${VENV_DIR} ${PYEXE})
+
+add_custom_target(cpp_python_gen
+  DEPENDS ${SIX_LOC})
