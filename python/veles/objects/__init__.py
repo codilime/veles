79ef1e64c4d2aa432123076ea11d026948ce648f
Jacek Świderski <jacek.swiderski@codilime.com>
2017-03-30 13:38:39 +0200

    Remove old approach to network communication using protobuf

--- python/veles/objects/__init__.py
+++
@@ -1,5 +0,0 @@
-from veles.objects.base import LocalObject
-from veles.objects.blob import Blob
-from veles.objects.chunk import Chunk
-
-__all__ = ['LocalObject', 'Blob', 'Chunk']
--- src/ui/optionsdialog.cc
+++ src/ui/optionsdialog.cc
@@ -18,7 +18,6 @@
 #include <QMessageBox>
 #include "ui_optionsdialog.h"
 #include "util/settings/hexedit.h"
-#include "util/settings/network.h"
 #include "util/settings/theme.h"
 
 namespace veles {
@@ -28,20 +27,10 @@ OptionsDialog::OptionsDialog(QWidget *parent)
     : QDialog(parent), ui(new Ui::OptionsDialog) {
   ui->setupUi(this);
   ui->colorsBox->addItems(util::settings::theme::availableIds());
-  QString ipRange = "(?:[0-1]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])";
-  QRegExp ipRegex("^" + ipRange + "\\." + ipRange +
-                  "\\." + ipRange + "\\." + ipRange + "$");
-  QRegExpValidator *ipValidator = new QRegExpValidator(ipRegex, ui->ipAddress);
-  ui->ipAddress->setValidator(ipValidator);
 
   connect(ui->hexColumnsAutoCheckBox, &QCheckBox::stateChanged,
           [this](int state) {
             ui->hexColumnsSpinBox->setEnabled(state != Qt::Checked);
-          });
-  connect(ui->networkEnabled, &QCheckBox::stateChanged,
-          [this](int state) {
-            ui->portBox->setEnabled(state == Qt::Checked);
-            ui->ipAddress->setEnabled(state == Qt::Checked);
           });
 }
 
@@ -57,12 +46,6 @@ void OptionsDialog::show() {
   ui->hexColumnsSpinBox->setValue(util::settings::hexedit::columnsNumber());
   ui->hexColumnsSpinBox->setEnabled(checkState != Qt::Checked);
 
-  ui->networkEnabled->setChecked(util::settings::network::enabled());
-  ui->ipAddress->setText(util::settings::network::ipAddress());
-  ui->ipAddress->setEnabled(ui->networkEnabled->isChecked());
-  ui->portBox->setValue(util::settings::network::port());
-  ui->portBox->setEnabled(ui->networkEnabled->isChecked());
-
   QWidget::show();
 }
 
@@ -77,24 +60,6 @@ void OptionsDialog::accept() {
   util::settings::hexedit::setResizeColumnsToWindowWidth(
       ui->hexColumnsAutoCheckBox->checkState() == Qt::Checked);
   util::settings::hexedit::setColumnsNumber(ui->hexColumnsSpinBox->value());
-
-  bool new_network = ui->networkEnabled->isChecked();
-  if (new_network != util::settings::network::enabled()) {
-    util::settings::network::setEnabled(new_network);
-    restart_needed = true;
-  }
-
-  QString new_ip = ui->ipAddress->text();
-  if (new_ip != util::settings::network::ipAddress()) {
-    util::settings::network::setIpAddress(new_ip);
-    restart_needed = true;
-  }
-
-  uint32_t new_port = ui->portBox->value();
-  if (new_port != util::settings::network::port()) {
-    util::settings::network::setPort(new_port);
-    restart_needed = true;
-  }
 
   if (restart_needed) {
     QMessageBox::about(
