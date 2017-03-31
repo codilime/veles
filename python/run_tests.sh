66a774c8ecd84cec3ea0451392b9e820bc779397
altran01 <jacek.swiderski@codilime.com>
2017-03-31 13:23:49 +0200

    Run Python unit tests on all platforms (#114)
    * Run Python unit tests on all platforms
    
    * Review fixes

--- python/run_tests.sh
+++ python/run_tests.sh
@@ -9,6 +9,3 @@ function finish {
 trap finish EXIT
 
 tox --workdir $dir
-$dir/py27/bin/testr last --subunit | subunit2junitxml --no-passthrough -o res27.xml
-$dir/py35/bin/testr last --subunit | subunit2junitxml --no-passthrough -o res35.xml
-
