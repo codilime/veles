a703f6c79e94411a3bade10227ac7737f3cc68df
Michał Kowalczyk <michal.kowalczyk@codilime.com>
2017-09-22 23:57:41 +0200

    Make variables in zlib.cmake uppercase

--- cmake/zlib.cmake
+++ cmake/zlib.cmake
@@ -7,44 +7,44 @@ if(ZLIB_FOUND)
 else()
   include(ExternalProject)
   # An external project for zlib
-  set(zlib_source  "${CMAKE_CURRENT_BINARY_DIR}/zlib-src")
-  set(zlib_build   "${CMAKE_CURRENT_BINARY_DIR}/zlib")
-  set(zlib_install "${CMAKE_CURRENT_BINARY_DIR}/prefix")
-  set(zlib_file "http://zlib.net/fossils/zlib-1.2.8.tar.gz")
-  set(zlib_md5 "44d667c142d7cda120332623eab69f40")
+  set(ZLIB_SOURCE  "${CMAKE_CURRENT_BINARY_DIR}/zlib-src")
+  set(ZLIB_BUILD   "${CMAKE_CURRENT_BINARY_DIR}/zlib")
+  set(ZLIB_INSTALL "${CMAKE_CURRENT_BINARY_DIR}/prefix")
+  set(ZLIB_FILE "http://zlib.net/fossils/zlib-1.2.8.tar.gz")
+  set(ZLIB_MD5 "44d667c142d7cda120332623eab69f40")
 
   if(CMAKE_CFG_INTDIR STREQUAL ".")
-    set(zlib_build_type ${CMAKE_BUILD_TYPE})
+    set(ZLIB_BUILD_TYPE ${CMAKE_BUILD_TYPE})
   else()
-    set(zlib_build_type ${CMAKE_CFG_INTDIR})
+    set(ZLIB_BUILD_TYPE ${CMAKE_CFG_INTDIR})
   endif()
 
   ExternalProject_Add(
       zlib
       DOWNLOAD_DIR ${CMAKE_CURRENT_BINARY_DIR}
-      SOURCE_DIR ${zlib_source}
-      BINARY_DIR ${zlib_build}
-      INSTALL_DIR ${zlib_install}
-      URL ${zlib_file}
-      URL_MD5 ${zlib_md5}
+      SOURCE_DIR ${ZLIB_SOURCE}
+      BINARY_DIR ${ZLIB_BUILD}
+      INSTALL_DIR ${ZLIB_INSTALL}
+      URL ${ZLIB_FILE}
+      URL_MD5 ${ZLIB_MD5}
       PATCH_COMMAND ${CMAKE_COMMAND} -E remove "<SOURCE_DIR>/zconf.h"
       CMAKE_CACHE_ARGS
       -DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS}
       -DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS}
-      -DCMAKE_BUILD_TYPE:STRING=${zlib_build_type}
-      ${zlib_EXTRA_ARGS}
+      -DCMAKE_BUILD_TYPE:STRING=${ZLIB_BUILD_TYPE}
+      ${ZLIB_EXTRA_ARGS}
       CMAKE_ARGS
       -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
   )
 
-  set(ZLIB_INCLUDE_DIRS "${zlib_install}/include/")
+  set(ZLIB_INCLUDE_DIRS "${ZLIB_INSTALL}/include/")
   set(ZLIB_INCLUDES "${ZLIB_INCLUDE_DIRS}/zconf.h" "${ZLIB_INCLUDE_DIRS}/zlib.h")
 
   if(MSVC)
-    set(ZLIB_LIBRARIES "${zlib_install}/lib/zlibstatic$<$<CONFIG:Debug>:d>.lib")
+    set(ZLIB_LIBRARIES "${ZLIB_INSTALL}/lib/zlibstatic$<$<CONFIG:Debug>:d>.lib")
   endif()
   if(UNIX)
-    set(ZLIB_LIBRARIES "${zlib_install}/lib/libz.a")
+    set(ZLIB_LIBRARIES "${ZLIB_INSTALL}/lib/libz.a")
   endif()
 endif()
 
