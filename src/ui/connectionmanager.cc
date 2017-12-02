20f04337e08c8bc833fad8684663449262e5e48a
Erik Potter <epottercg@gmail.com>
2017-12-01 19:24:23 -0600

    FreeBSD support
    Tested on FreeBSD 11.0-RELEASE

--- src/ui/connectionmanager.cc
+++ src/ui/connectionmanager.cc
@@ -220,10 +220,16 @@ void ConnectionManager::startLocalServer() {
   if (!check_file.exists()) {
     python_interpreter_executable = QString("python3");
   }
+#elif defined(Q_OS_FREEBSD)
+  QString python_interpreter_executable("/usr/local/bin/python3");
+  QFileInfo check_file(python_interpreter_executable);
+  if (!check_file.exists()) {
+    python_interpreter_executable = QString("python3");
+  }
 #else
 #warning \
     "This OS is not officially supported, you may need to set this string manually."
-  python_interpreter_executable = QString("python3");
+  QString python_interpreter_executable("python3");
 #endif
 
   QTextStream out(veles::ui::LogWidget::output());
